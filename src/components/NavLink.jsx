ca9b37e095ee0e5f8fc6e2626357c8335cc7e71e
Pedro Molina <pedro.molinanoa@gmail.com>
2023-12-25 02:50:19 -0500

    Navbar active

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavLink = ({ href, title }) => {
+  const pathname = usePathname();
+
   return (
     <Link
       href={href}
-      className="block  py-2 pl-3 pr-4 text-[#ADB7BE]  sm:text-base rounded md:p-0 hover:text-white"
+      className={`link ${
+        pathname === href ? "active" : ""
+      } block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-base rounded md:p-0 hover:text-white`}
+      style={{
+        borderBottom: pathname === href ? "2px solid #f9eb37" : "none",
+        borderRadius: 0,
+      }}
     >
       {title}
     </Link>
   );
 };
+
 export default NavLink;

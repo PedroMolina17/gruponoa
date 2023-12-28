ea26653df1b2003c535eaee7c54c50df3fc10bbf
Pedro Molina <pedro.molinanoa@gmail.com>
2023-12-28 02:57:38 -0500

    Card

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -10,43 +10,49 @@ const productData = [
     id: 1,
     title: "Melamina 1",
     price: "S/350.00",
+    extend: "1500 x 1500mm / 18 mm",
     imgUrl: "/img/bg_1.jpeg",
-    tag: ["Certificates", "All"],
+    tag: ["office", "All"],
   },
   {
     id: 2,
     title: "Melamina 2",
     price: "S/350.00",
+    extend: "1500 x 1500mm / 18 mm",
     imgUrl: "/img/bg_1.jpeg",
-    tag: ["Certificates", "All"],
+    tag: ["office", "All"],
   },
   {
     id: 3,
     title: "Melamina 2",
     price: "S/350.00",
+    extend: "1500 x 1500mm / 18 mm",
     imgUrl: "/img/bg_1.jpeg",
-    tag: ["Certificates", "All"],
+    tag: ["office", "All"],
   },
   {
     id: 4,
     title: "Melamina 2",
     price: "S/350.00",
+    extend: "1500 x 1500mm / 18 mm",
     imgUrl: "/img/bg_1.jpeg",
-    tag: ["Projects", "All"],
+    tag: ["office", "All"],
   },
   {
     id: 5,
     title: "Melamina 2",
     price: "S/350.00",
+    extend: "1500 x 1500mm / 18 mm",
     imgUrl: "/img/bg_1.jpeg",
-    tag: ["All", "Certificates"],
+    tag: ["All", "bedroom"],
   },
   {
     id: 6,
     title: "Melamina 2",
     price: "S/350.00",
+    extend: "1500 x 1500mm / 18 mm",
     imgUrl: "/img/bg_1.jpeg",
-    tag: ["All", "Certificates"],
+    tag: ["All", "bedroom"],
   },
 ];
 
@@ -69,7 +75,7 @@ const Products = () => {
   };
   return (
     <section id="projects" className="bg-white py-4 ">
-      <div className="bg-white opacity-90 p-8 flex justify-center">
+      <div className="bg-white opacity-90 p-4 flex justify-center">
         <div className="flex w-3/4  border-b text-black border-gray-300 p-2 ">
           <input
             type="text"
@@ -83,7 +89,7 @@ const Products = () => {
       </div>
 
       <div className="md:grid md:grid-cols-5 text-black  bg-white opacity-90 ">
-        <div className="col-span-1 py-16 ">
+        <div className="col-span-1 py-1 ">
           <div className="text-black flex flex-col justify-center lg:items-start  mx-9 gap-2 py-6 ">
             <ProductsTag
               onClick={handleTabChange}
@@ -94,15 +100,15 @@ const Products = () => {
             <div className="text-black flex flex-col justify-center lg:items-start mx-4 gap-2 py-1 ">
               <ProductsTag
                 onClick={handleTabChange}
-                name="Projects"
+                name="bedroom"
                 description="Dormitorio"
-                isSelected={tag === "Projects"}
+                isSelected={tag === "bedroom"}
               />
               <ProductsTag
                 onClick={handleTabChange}
-                description="Oficina  "
-                name="Certificates"
-                isSelected={tag === "Certificates"}
+                description="Oficina"
+                name="office"
+                isSelected={tag === "office"}
               />
             </div>
           </div>
@@ -124,8 +130,8 @@ const Products = () => {
                 key={project.id}
                 title={project.title}
                 price={project.price}
+                extend={project.extend}
                 imgUrl={project.imgUrl}
-                isCertificate={project.tag.includes("Certificates")}
               />
             </motion.li>
           ))}
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-const ProjectCard = ({ imgUrl, title, price }) => {
+const ProjectCard = ({ imgUrl, title, price, extend }) => {
   return (
     <div>
-      <div
-        className="h-52 md:h-72 rounded-t-xl"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      ></div>
-      <div className="text-black text-center bg-[#f9eb37] rounded-b-xl">
-        <h5 className="py-1 opacity-90">{title}</h5>
-        <p className="text-lg opacity-90">{price}</p>
+      <div className="border-2 flex justify-center rounded-t-xl">
+        <div
+          className="h-52 md:h-72  border w-4/5 my-1"
+          style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+        ></div>
+      </div>
+
+      <div className="text-black text-start border-b-2 border-r-2 border-l-2  rounded-b-xl">
+        <h1 className="text-xl py-1 opacity-90 px-1">{title}</h1>
+        <p className="text-sm opacity-90 py-1 px-1">{extend}</p>
+        <p className="text-sm opacity-90 pb-1 px-1">{price}</p>
       </div>
     </div>
   );

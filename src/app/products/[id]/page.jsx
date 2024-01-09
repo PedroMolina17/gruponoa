9d6acdf47999d2ebafbcc871819b58b4ced9bd11
Pedro Molina <pedro.molinanoa@gmail.com>
2024-01-09 00:07:22 -0500

    Details

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 import { useSelector } from "react-redux";
 const Page = ({ params }) => {
   const minombre = useSelector(
-    (state) => state.valores.productData[params.id - 1]
+    (state) => state.valores.productData[params.id - 1].title
   );
 
   return (
     <div className="h-5/6 w-full bg-white text-black">
       page {params.id}
-      <div className="text-black">{minombre.title}</div>
+      <div className="text-black">{minombre}</div>
     </div>
   );
 };
